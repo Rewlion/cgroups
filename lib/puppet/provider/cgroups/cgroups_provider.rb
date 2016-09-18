83506e1561c9582686d9a859187f1c43ff80eac1
Rewlion <alexxX249@gmail.com>
2016-09-19 00:14:55 +0300

    put properties through type argumment

--- lib/puppet/provider/cgroups/cgroups_provider.rb
+++ lib/puppet/provider/cgroups/cgroups_provider.rb
@@ -1,55 +1,40 @@
 Puppet::Type.type(:cgroups).provide(:cgroups_provider) do 
-	desc 'cgroups provider for properties setting'
+    desc 'cgroups provider for properties setting'
 
-	def self.set_properties
-		property_hash[:properties].each |property, value| do
-			unless value.nil?
-				property = property + "=" + value
-				set_property(property) 
-			end
-		end
-	end
+    def self.set_properties
+        property_hash[:properties].each |property| do
+            set_property(property)
+        end
+    end
 
 
 
-	def self.set_property( property )
-		service = @property_hash[:name] + ".service"
-		cmd = "systemctl set-property " + service + " " + property
-		system(cmd)
-	end
+    def self.set_property( property )
+        service = @property_hash[:name] + ".service"
+        cmd = "systemctl set-property " + service + " " + property
+        system(cmd)
+    end
 
 
 
-	def create
-		@property_hash[:properties] = {
-			CPUShares				=> @resource[CPUShares] 			, 
-			MemoryLimit				=> @resource[MemoryLimit	] 		,
-			BlockIOWeight 			=> @resource[BlockIOWeight] 		,
-			BlockIODeviceWeight 	=> @resource[BlockIODeviceWeight ] 	,
-			BlockIOReadBandwidth	=> @resource[BlockIOReadBandwidth] 	,
-			BlockIOWriteBandwidth	=> @resource[BlockIOWriteBandwidth] ,
-			DeviceAllow				=> @resource[DeviceAllow] 			,
-			DevicePolicy			=> @resource[DevicePolicy] 			,
-			Slice					=> @resource[Slice] 				,
-			ControlGroupAttribute	=> @resource[ControlGroupAttribute] 
-		}
-		@property_hash[:name]	= :resource[:name]
-		set_properties
-		@property_hash[:ensure] = :present
+    def create
+        @property_hash[:properties] = @resource[:properties]
+        @property_hash[:name]       = @resource[:name]
+        set_properties
+        @property_hash[:ensure]     = :present
 
-		exist?
-
-	end
+        exist?
+    end
 
 
 
-	def destroy
-	
-	end
+    def destroy
+    
+    end
 
 
 
-	def exist?
-		 @property_hash[:ensure] == :present
-	end
+    def exist?
+         @property_hash[:ensure] == :present
+    end
 end
