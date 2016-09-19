a4772cf861662a388716722c5b3144f98b5d2fb2
Rewlion <alexxX249@gmail.com>
2016-09-19 22:23:43 +0300

    converted tab to spaces

--- spec/classes/cgroups_spec.rb
+++ spec/classes/cgroups_spec.rb
@@ -2,25 +2,25 @@ require 'spec_helper'
 require 'puppet/type/cgroups.rb'
 
 describe 'cgroups', :type => :class do
-	
-	context "On RedHat" do
-		
-		let (:facts) {{
-					:osfamily => "redhat",
-					:operatingsystem => "CentOS"
-				}}
+    
+    context "On RedHat" do
+        
+        let (:facts) {{
+                    :osfamily => "redhat",
+                    :operatingsystem => "CentOS"
+                }}
 
-		let (:params) do 
-			{ :cgroups_set => {  
-								'Apache' => { 'properties' => [ "CPUShares=600", "MemoryLimit=500M" ] } 
-							 } }
-		end
+        let (:params) do 
+            { :cgroups_set => {  
+                                'Apache' => { 'properties' => [ "CPUShares=600", "MemoryLimit=500M" ] } 
+                             } }
+        end
 
-		it{ is_expected.to compile }
-		
-		it { should contain_class('cgroups').with(:cgroups_set => params[:cgroups_set] )	}
-	
-		it{ should contain_cgroups('Apache').with({ 'properties' => [ "CPUShares=600", "MemoryLimit=500M" ] } ) }
-	end
+        it{ is_expected.to compile }
+        
+        it { should contain_class('cgroups').with(:cgroups_set => params[:cgroups_set] )    }
+    
+        it{ should contain_cgroups('Apache').with({ 'properties' => [ "CPUShares=600", "MemoryLimit=500M" ] } ) }
+    end
 
 end
