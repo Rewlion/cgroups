df86d3eae802312787513635e6f0c2b11727fc83
Alexey Gladkov <alexxX249@gmail.com>
2016-09-25 16:29:53 +0300

    added validation of input args

--- lib/puppet/parser/functions/parse_cgroups_json.rb
+++ lib/puppet/parser/functions/parse_cgroups_json.rb
@@ -1,5 +1,111 @@
 require 'json'
 
+
+
+
+def isStrBool?(str)
+  str.to_s == "true" or str.to_s == "false"
+end
+
+
+
+def isValidSharesValue?(value)
+  if value.to_s =~ /^[0-9]+$/
+    return (2..262144) === value.to_s.to_i
+  else
+    false
+  end
+end
+
+
+
+def isValidCPUQuotaValue?(value)
+   value.to_s =~ /^[0-9]+%$/
+end
+
+
+
+def isValidMemoryValue?(value)
+   value.to_s =~ /^[0-9]+[K|M|G|T]?$/
+end
+
+
+
+
+def isValidTaskMaxValue?(value)
+   value.to_s =~ /^[0-9]+$/
+end
+
+
+
+
+def isValidBlockIODeviceWeightValue?(value)
+  if value =~ /^\/dev\/.+\s+[0-9]+$/
+    return (1..1000) === value.split[1].to_i
+  else
+    return false
+  end
+end
+
+
+
+def isDeviceBytesValue?(value)
+  value =~ /^\/dev\/.+\s+[0-9]+[K|M|G|T]?$/
+end
+
+
+
+def isValidDeviceAllowValue?(value)
+  value =~ /^\/dev\/.+\s+[r|w|x]?$/
+end
+
+
+def isValidDevicePolicyValue?(value)
+  value.to_s =~ /strict|auto|closed/
+end
+
+
+def validateOption( service, property, value ) 
+  #raise ("#{service}::#{property}::#{value} is not a string type") unless value.is_a?(String)
+
+  case property
+    when "CPUAccounting", "MemoryAccounting", "TasksAccounting", "BlockIOAccounting"
+      raise ("#{service}::#{property}::#{value} is not valid") unless isStrBool? value
+    
+    when "CPUShares", "StartupCPUShares"
+      raise ("#{service}::#{property}::#{value} is not valid ") unless isValidSharesValue? value 
+
+    when "CPUQuota"
+      raise ("#{service}::#{property}::#{value} is not valid") unless isValidCPUQuotaValue? value 
+    
+    when "MemoryLimit"
+      raise ("#{service}::#{property}::#{value} is not valid") unless isValidMemoryValue? value
+   
+    when "TasksMax"
+      raise ("#{service}::#{property}::#{value} is not valid") unless isValidTaskMaxValue? value
+    
+    when "BlockIODeviceWeight" 
+      raise ("#{service}::#{property}::#{value} is not valid") unless isValidBlockIODeviceWeightValue? value
+    
+    when "BlockIOReadBandwidth", "BlockIOWriteBandwidth" 
+      raise ("#{service}::#{property}::#{value} is not valid") unless isDeviceBytesValue? value 
+    
+    when "DeviceAllow"
+      raise ("#{service}::#{property}::#{value} is not valid") unless isValidDeviceAllowValue? value 
+    
+    when "DevicePolicy"
+      raise ("#{service}::#{property}::#{value} is not valid") unless isValidDevicePolicyValue? value
+    
+    when "Slice" , "Delegate"
+    
+    else
+      raise("#{service}::#{property} is not supported")
+    end
+end
+
+
+
+
 def parse_properties(service, json)
 	options = JSON.parse(json) rescue raise("#{service} JSON parsing  error for : #{json}")
 
@@ -8,13 +114,14 @@ def parse_properties(service, json)
 	end
 
 	options.each do |property, value|
-		unless value.is_a?(Integer) or value.is_a?(String) 
-			raise(Puppet::ParseError ,"#{service}::#{property}::#{value} is not a string or Int")
-		end
+	 validateOption(service, property, value)
 	end
 
 	options
 end
+
+
+
 
 Puppet::Parser::Functions::newfunction(:parse_cgroups_json, :type => :rvalue,  :doc => <<-EOS
     This functions parses an input hash with metadata and cgroups settings(*) 
--- spec/functions/parse_cgroups_json_spec.rb
+++ spec/functions/parse_cgroups_json_spec.rb
@@ -3,31 +3,6 @@ require 'spec_helper'
 
 describe 'parse_cgroups_json' do 
 
-	context 'parse valid hash data' do 
-		let(:params) {
-      	{
-        	'metadata' => {
-          	'always_editable' => true,
-          	'group' => 'general',
-          	'label' => 'Cgroups',
-          	'weight' => 50
-        	},
-        	"cinder-api" 	=>  '{"BlockIOWeight":500, "MemoryLimit":"1G"}',
-        	"apache"		=> 	'{"CPUShares":1200}'
-      	} }
-
-      	let(:result) {
-        {
-        	"cinder-api"  => { "BlockIOWeight" => 500, "MemoryLimit" => "1G" },
-       	 	"apache" 	  => { "CPUShares" => 1200 } 
-       	}}
-		
-
-       	it 'should parse valid hash' do
-      		is_expected.to run.with_params(params).and_return(result)
-		end
-	end
-
 	context "wrong JSON format" do
 
     	let(:params) {
@@ -43,26 +18,421 @@ describe 'parse_cgroups_json' do
 	context "should not parse a data with type different from Hash" do
 
     	let(:params) {
-        	'cinder-api:{"BlockIOWeight":500, "MemoryLimit":"1G"}'
+        	'cinder-api:{"TasksMax":500, "MemoryLimit":"1G"}'
     	}
 
     	it 'should raise if group option is not a Hash' do
-      		is_expected.to run.with_params(params).and_raise_error(Puppet::ParseError)
+      		is_expected.to run.with_params(params).and_raise_error(RuntimeError)
 		end
 	end
 
-	context "should not parse a data with property value's type different from int and string" do
+	context "should not parse a data with property value's type different from string,int" do
 
     	let(:params) {
       	{
-        	"cinder-api" => '{"BlockIOWeight":[500,1]}'
+        	"cinder-api" => '{"TasksMax":[500,1]}'
       	}
     	}
 
     	it 'should raise if property value is an array' do
-      		is_expected.to run.with_params(params).and_raise_error(Puppet::ParseError)
+      		is_expected.to run.with_params(params).and_raise_error(RuntimeError)
 		end
 	end
 
-	 
-end
+	###########################
+  context 'CPUAccounting with bool value' do 
+    let(:params) {
+        {
+          'metadata' => {
+            'always_editable' => true,
+            'group' => 'general',
+            'label' => 'Cgroups',
+            'weight' => 50
+          },
+          "cinder-api"  =>  '{"CPUAccounting" : "true"}'
+        } }
+
+        let(:result) {
+        {
+          "cinder-api"  => { "CPUAccounting" => "true" },
+        }}
+    
+
+        it 'should parse valid hash' do
+          is_expected.to run.with_params(params).and_return(result)
+    end
+  end
+
+
+  context "CPUAccounting with int value" do
+
+      let(:params) {
+        {
+          "cinder-api" => '{"CPUAccounting":"200"}'
+        }
+      }
+
+      it  do
+          is_expected.to run.with_params(params).and_raise_error(RuntimeError)
+    end
+  end
+
+
+  context 'CPUShares with valid value' do 
+    let(:params) {
+        {
+          'metadata' => {
+            'always_editable' => true,
+            'group' => 'general',
+            'label' => 'Cgroups',
+            'weight' => 50
+          },
+          "cinder-api"  =>  '{"CPUShares" : "300"}'
+        } }
+
+        let(:result) {
+        {
+          "cinder-api"  => { "CPUShares" => "300" },
+        }}
+    
+
+        it 'should parse valid hash' do
+          is_expected.to run.with_params(params).and_return(result)
+    end
+  end
+
+
+context "CPUShares with invalid value" do
+
+      let(:params) {
+        {
+          "cinder-api" => '{"CPUShares":"FOO"}'
+        }
+      }
+
+      it  do
+          is_expected.to run.with_params(params).and_raise_error(RuntimeError)
+    end
+  end
+
+  context "CPUShares with invalid value(lover min)" do
+
+      let(:params) {
+        {
+          "cinder-api" => '{"CPUShares":"1"}'
+        }
+      }
+
+      it  do
+          is_expected.to run.with_params(params).and_raise_error(RuntimeError)
+    end
+  end
+
+
+  context "CPUShares with invalid value(bigger max)" do
+
+      let(:params) {
+        {
+          "cinder-api" => '{"CPUShares":"300000"}'
+        }
+      }
+
+      it  do
+          is_expected.to run.with_params(params).and_raise_error(RuntimeError)
+    end
+  end
+
+
+    context 'CPUQuota with valid value' do 
+    let(:params) {
+        {
+          'metadata' => {
+            'always_editable' => true,
+            'group' => 'general',
+            'label' => 'Cgroups',
+            'weight' => 50
+          },
+          "cinder-api"  =>  '{"CPUQuota" : "50%"}'
+        } }
+
+        let(:result) {
+        {
+          "cinder-api"  => { "CPUQuota" => "50%" },
+        }}
+    
+
+        it 'should parse valid hash' do
+          is_expected.to run.with_params(params).and_return(result)
+    end
+  end
+
+
+    context 'CPUQuota with valid value' do
+
+      let(:params) {
+        {
+          "cinder-api" => '{"CPUQuota":"200"}'
+        }
+      }
+
+      it  do
+          is_expected.to run.with_params(params).and_raise_error(RuntimeError)
+    end
+  end
+
+
+    context 'MemoryLimit with valid value' do 
+    let(:params) {
+        {
+          'metadata' => {
+            'always_editable' => true,
+            'group' => 'general',
+            'label' => 'Cgroups',
+            'weight' => 50
+          },
+          "cinder-api"  =>  '{"MemoryLimit" : "1024M"}'
+        } }
+
+        let(:result) {
+        {
+          "cinder-api"  => { "MemoryLimit" => "1024M" },
+        }}
+    
+
+        it 'should parse valid hash' do
+          is_expected.to run.with_params(params).and_return(result)
+    end
+  end
+
+
+context "MemoryLimit with invalid value" do
+
+      let(:params) {
+        {
+          "cinder-api" => '{"MemoryLimit":"200FOO"}'
+        }
+      }
+
+      it  do
+          is_expected.to run.with_params(params).and_raise_error(RuntimeError)
+    end
+  end
+
+
+    context 'TasksMax with valid value' do 
+    let(:params) {
+        {
+          'metadata' => {
+            'always_editable' => true,
+            'group' => 'general',
+            'label' => 'Cgroups',
+            'weight' => 50
+          },
+          "cinder-api"  =>  '{"TasksMax" : "22"}'
+        } }
+
+        let(:result) {
+        {
+          "cinder-api"  => { "TasksMax" => "22" },
+        }}
+    
+
+        it 'should parse valid hash' do
+          is_expected.to run.with_params(params).and_return(result)
+    end
+  end
+
+
+context "TasksMax with invalid value" do
+
+      let(:params) {
+        {
+          "cinder-api" => '{"TasksMax":"FOO"}'
+        }
+      }
+
+      it  do
+          is_expected.to run.with_params(params).and_raise_error(RuntimeError)
+    end
+  end
+
+
+    context 'BlockIODeviceWeight with valid value' do 
+    let(:params) {
+        {
+          'metadata' => {
+            'always_editable' => true,
+            'group' => 'general',
+            'label' => 'Cgroups',
+            'weight' => 50
+          },
+          "cinder-api"  =>  '{"BlockIODeviceWeight" : "/dev/sda1 10"}'
+        } }
+
+        let(:result) {
+        {
+          "cinder-api"  => { "BlockIODeviceWeight" => "/dev/sda1 10" },
+        }}
+    
+
+        it 'should parse valid hash' do
+          is_expected.to run.with_params(params).and_return(result)
+    end
+  end
+
+
+context "BlockIODeviceWeight with invalid value by syntax" do
+
+      let(:params) {
+        {
+          "cinder-api" => '{"BlockIODeviceWeight":"foo"}'
+        }
+      }
+
+      it  do
+          is_expected.to run.with_params(params).and_raise_error(RuntimeError)
+    end
+  end
+
+context "BlockIODeviceWeight with invalid value( > 1k )" do
+
+      let(:params) {
+        {
+          "cinder-api" => '{"BlockIODeviceWeight":"1000000"}'
+        }
+      }
+
+      it  do
+          is_expected.to run.with_params(params).and_raise_error(RuntimeError)
+    end
+  end
+
+context "BlockIODeviceWeight with invalid value( < 10 )" do
+
+      let(:params) {
+        {
+          "cinder-api" => '{"BlockIODeviceWeight":"0"}'
+        }
+      }
+
+      it  do
+          is_expected.to run.with_params(params).and_raise_error(RuntimeError)
+    end
+  end
+
+
+context 'BlockIOReadBandwidth with valid value' do 
+    let(:params) {
+        {
+          'metadata' => {
+            'always_editable' => true,
+            'group' => 'general',
+            'label' => 'Cgroups',
+            'weight' => 50
+          },
+          "cinder-api"  =>  '{"BlockIOReadBandwidth" : "/dev/disk/by-path/pci-0000:00:1f.2-scsi-0:0:0:0 5M"}'
+        } }
+
+        let(:result) {
+        {
+          "cinder-api"  => { "BlockIOReadBandwidth" => "/dev/disk/by-path/pci-0000:00:1f.2-scsi-0:0:0:0 5M" },
+        }}
+    
+
+        it 'should parse valid hash' do
+          is_expected.to run.with_params(params).and_return(result)
+    end
+  end
+
+
+context "BlockIOReadBandwidth with invalid value" do
+
+      let(:params) {
+        {
+          "cinder-api" => '{"CPUAccounting":"foo"}'
+        }
+      }
+
+      it  do
+          is_expected.to run.with_params(params).and_raise_error(RuntimeError)
+    end
+  end
+
+
+context 'DeviceAllow with valid value' do 
+    let(:params) {
+        {
+          'metadata' => {
+            'always_editable' => true,
+            'group' => 'general',
+            'label' => 'Cgroups',
+            'weight' => 50
+          },
+          "cinder-api"  =>  '{"DeviceAllow" : "/dev/sdb1 r"}'
+        } }
+
+        let(:result) {
+        {
+          "cinder-api"  => { "DeviceAllow" => "/dev/sdb1 r" },
+        }}
+    
+
+        it 'should parse valid hash' do
+          is_expected.to run.with_params(params).and_return(result)
+    end
+  end
+
+
+context "DevicePolicy with valid value" do
+    let(:params) {
+        {
+          'metadata' => {
+            'always_editable' => true,
+            'group' => 'general',
+            'label' => 'Cgroups',
+            'weight' => 50
+          },
+          "cinder-api" => '{"DevicePolicy" : "strict"}'
+        } }
+
+        let(:result) {
+        {
+          "cinder-api"  => { "DevicePolicy" => "strict" },
+        }}
+    
+
+        it 'should parse valid hash' do
+          is_expected.to run.with_params(params).and_return(result)
+    end
+  end
+
+context "DevicePolicy with invalid value" do
+
+      let(:params) {
+        {
+          "cinder-api" => '{"DevicePolicy" : "FOO"}'
+        }
+      }
+
+      it  do
+          is_expected.to run.with_params(params).and_raise_error(RuntimeError)
+    end
+  end
+
+
+
+context "Unknown property" do
+
+      let(:params) {
+        {
+          "cinder-api" => '{"UNKNOWN" : "FOO"}'
+        }
+      }
+
+      it  do
+          is_expected.to run.with_params(params).and_raise_error(RuntimeError)
+    end
+  end
+
+
+ end
