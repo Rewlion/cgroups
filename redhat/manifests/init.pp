8a187e0499dd52ae7f7857b3b10d84f3d4138bbc
Alexey Gladkov <alexxX249@gmail.com>
2016-09-27 18:56:17 +0300

    refactoring cgroups >cgroups::redhat

---
+++ redhat/manifests/init.pp
@@ -0,0 +1,13 @@
+class cgroups::redhat( $cgroups_set = {} ) {
+
+    case $::osfamily {
+        'redhat': {
+                $cgroups_set.each |$service, $properties| {
+                    $properties.each |$property,$value| {
+                        exec {"/usr/bin/systemctl set-property ${service}.service ${property}=${value}"  :}
+                    }
+                } 
+            }
+        default: { fail('Unsupported OS') }
+    }
+}
